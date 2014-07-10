5bb6aad42d58a667285ad61b79173902a315d1d6
Colin Basnett <cmbasnett@gmail.com>
2014-07-10 01:01:25 -0700

    fix for make compiling packages it didn't need to

--- tools/make/main.cpp
+++ tools/make/main.cpp
@@ -9,11 +9,21 @@
 #include <boost\filesystem.hpp>
 #include <boost\lexical_cast.hpp>
 
+#define EXIT(RESULT)\
+    if(!should_close_on_finish)\
+    {\
+        system("PAUSE");\
+    }\
+    \
+    return RESULT;
+
 int main(int argc, char** argv)
 {
     using namespace boost::filesystem;
 
     std::string mod_name;
+    bool is_silent = false;
+    bool should_close_on_finish = false;
 
     for (auto i = 0; i < argc; ++i)
     {
@@ -27,13 +37,17 @@ int main(int argc, char** argv)
                 mod_name = tokens[1];
             }
         }
+        else if (_strcmpi(argv[i], "--close") == 0)
+        {
+            should_close_on_finish = true;
+        }
     }
 
     if (mod_name.empty())
     {
         std::cerr << "ERROR: mod not specified (use -mod=<mod_name>)" << std::endl;
 
-        return 1;
+        EXIT(1);
     }
 
     size_t size = 0;
@@ -43,31 +57,31 @@ int main(int argc, char** argv)
 
     if (rodir == nullptr)
     {
-        std::cerr << "ERROR: Environment variable 'RODIR' is not set." << std::endl;
+        std::cerr << "ERROR: Environment variable RODIR is not set." << std::endl;
 
-        return 1;
+        EXIT(1);
     }
 
-    path red_orchestra_root_directory = rodir;
+    path root_directory = rodir;
 
-    if (!is_directory(red_orchestra_root_directory))
+    if (!is_directory(root_directory))
     {
-        std::cerr << "ERROR: 'RODIR' (" << red_orchestra_root_directory << ") is not a directory." << std::endl;
+        std::cerr << "ERROR: RODIR (" << root_directory << ") is not a directory." << std::endl;
 
-        return 1;
+        EXIT(1);
     }
 
-    path red_orchestra_system_directory = red_orchestra_root_directory;
-    red_orchestra_system_directory += "\\System";
+    path root_system_directory = root_directory;
+    root_system_directory += "\\System";
 
-    auto mod_directory = red_orchestra_root_directory;
+    auto mod_directory = root_directory;
     mod_directory += "\\" + mod_name;
 
     if (!exists(mod_directory))
     {
         std::cerr << "ERROR: Mod directory (" << mod_directory << ") does not exist." << std::endl;
 
-        return 1;
+        EXIT(1);
     }
 
     auto mod_system_directory = mod_directory;
@@ -77,7 +91,7 @@ int main(int argc, char** argv)
     {
         std::cerr << "ERROR: Mod System directory (" << mod_system_directory << ") does not exist." << std::endl;
 
-        return 1;
+        EXIT(1);
     }
 
     auto mod_ini_path = mod_system_directory;
@@ -87,7 +101,7 @@ int main(int argc, char** argv)
     {
         std::cerr << "ERROR: mod .ini (" << mod_name << ") does not exist.";
 
-        return 1;
+        EXIT(1);
     }
 
     std::ifstream mod_ini_stream;
@@ -112,42 +126,42 @@ int main(int argc, char** argv)
             {
                 std::cerr << "ERROR: invalid 'EditPackages' entry in .ini.";
 
-                return 1;
+                EXIT(1);
             }
 
-            //if (boost::algorithm::starts_with(tokens[1], "DH"))
-            {
-                edit_packages.push_back(tokens[1]);
-            }
+            edit_packages.push_back(tokens[1]);
         }
     }
 
     std::vector<std::string> packages_to_compile;
 
-    std::map<boost::filesystem::path, time_t> package_write_times;
-
 	for (size_t i = 0; i < edit_packages.size(); ++i)
     {
 		const auto& edit_package = edit_packages[i];
 
-        auto src_dir = red_orchestra_root_directory;
+        auto src_dir = root_directory;
         src_dir += "\\" + edit_package + "\\Classes";
 
         if (!is_directory(src_dir))
         {
-            std::cerr << "WARNING: no such source directory (\"" << src_dir << "\").";
+            std::cerr << "WARNING: no such source directory (\"" << src_dir << "\")." << std::endl;
 
             continue;
         }
 
-        path package_path = mod_system_directory;
-        package_path += "\\" + edit_package + ".u";
+        //build path for package in mod system directory
+        path mod_package_path = mod_system_directory;
+        mod_package_path += "\\" + edit_package + ".u";
 
-        if (exists(package_path))
+        //build path for package in root 
+        path root_package_path = root_system_directory;
+        root_package_path += "\\" + edit_package + ".u";
+
+        if (exists(mod_package_path))
         {
             for (auto itr = directory_iterator(src_dir); itr != directory_iterator(); ++itr)
             {
-                if (last_write_time(itr->path()) > last_write_time(package_path))
+                if (last_write_time(itr->path()) > last_write_time(mod_package_path))
                 {
                     packages_to_compile.push_back(edit_package);
 
@@ -155,7 +169,7 @@ int main(int argc, char** argv)
                 }
             }
         }
-        else
+        else if (!exists(root_package_path))
         {
             packages_to_compile.push_back(edit_package);
         }
@@ -165,12 +179,14 @@ int main(int argc, char** argv)
     {
         std::cout << "no packages to compile" << std::endl;
 
-        return 1;
+        EXIT(1);
     }
 	
 	for (size_t i = 0; i < packages_to_compile.size(); ++i)
     {
 		const auto& package = packages_to_compile[i];
+
+        std::cout << "\"" << package << "\" marked for building." << std::endl;
 
         path package_path = mod_system_directory;
         package_path += "\\" + package;
@@ -186,9 +202,7 @@ int main(int argc, char** argv)
             {
                 std::cerr << "ERROR: unable to delete package \"" << package_path.filename() << "\" (is it open in another process?)" << std::endl;
 
-                system("PAUSE");
-
-                return 1;
+                EXIT(1);
             }
         }
     }
@@ -206,7 +220,7 @@ int main(int argc, char** argv)
     }
 
     //copy compiled .u files to mod system directory and delete them
-    for (auto itr = directory_iterator(red_orchestra_system_directory); itr != directory_iterator(); ++itr)
+    for (auto itr = directory_iterator(root_system_directory); itr != directory_iterator(); ++itr)
     {
 		for (size_t i = 0; i < packages_to_compile.size(); ++i)
 		{
@@ -224,4 +238,6 @@ int main(int argc, char** argv)
             }
         }
     }
+
+    EXIT(0);
 }
