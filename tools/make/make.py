ec481ec7c1debbad80b6716e0f427a60c936e3c5
Colin Basnett <cmbasnett@gmail.com>
2015-04-05 23:07:26 +0100

    updated make.py to evaluate package contents and compare a stored CRC value, this will allow make.py to mark packages for recompile after removing a .uc file

--- tools/make/make.py
+++ tools/make/make.py
@@ -5,6 +5,8 @@ import subprocess
 import time
 import ConfigParser
 import shutil
+import json
+from binascii import crc32
 from datetime import datetime
 from collections import OrderedDict
 
@@ -16,138 +18,152 @@ class MultiOrderedDict(OrderedDict):
             super(OrderedDict, self).__setitem__(key, value)
 
 def main():
-	#red orchestra directory
-	ro_dir = os.environ.get('RODIR')
+    #red orchestra directory
+    ro_dir = os.environ.get('RODIR')
 
-	if ro_dir == None:
-		print 'error: environment variable RODIR is not defined'
-		sys.exit(1)
+    if ro_dir == None:
+        print 'error: environment variable RODIR is not defined'
+        sys.exit(1)
 
-	if not os.path.isdir(ro_dir):
-		print 'error: environment variable RODIR is not a valid directory'
-		sys.exit(1)
+    if not os.path.isdir(ro_dir):
+        print 'error: environment variable RODIR is not a valid directory'
+        sys.exit(1)
 
-	#red orchestra system directory
-	ro_sys_dir = os.path.join(ro_dir, 'System')
+    #red orchestra system directory
+    ro_sys_dir = os.path.join(ro_dir, 'System')
 
-	if not os.path.isdir(ro_sys_dir):
-		print 'error: could not resolve red orchestra system directory'
-		sys.exit(1)
+    if not os.path.isdir(ro_sys_dir):
+        print 'error: could not resolve red orchestra system directory'
+        sys.exit(1)
 
-	#parse options
-	argparser = argparse.ArgumentParser()
-	argparser.add_argument('-mod', required=True)
-	argparser.add_argument('-clean', required=False, action='store_true')
-	argparser.add_argument('-dumpint', required=False, action='store_true')
-	args = argparser.parse_args()
+    #parse options
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-mod', required=True)
+    argparser.add_argument('-clean', required=False, action='store_true')
+    argparser.add_argument('-dumpint', required=False, action='store_true')
+    args = argparser.parse_args()
 
-	#mod directory
-	mod_dir = os.path.join(ro_dir, args.mod)
+    #mod directory
+    mod_dir = os.path.join(ro_dir, args.mod)
 
-	if not os.path.isdir(mod_dir):
-		print 'error: could not resolve mod directory'
-		sys.exit(1)
+    if not os.path.isdir(mod_dir):
+        print 'error: could not resolve mod directory'
+        sys.exit(1)
 
-	#mod system directory
-	mod_sys_dir = os.path.join(mod_dir, 'System')
+    #mod system directory
+    mod_sys_dir = os.path.join(mod_dir, 'System')
 
-	if not os.path.isdir(mod_sys_dir):
-		print 'error could not resolve mod system directory'
-		sys.exit(1)
+    if not os.path.isdir(mod_sys_dir):
+        print 'error could not resolve mod system directory'
+        sys.exit(1)
 
-	#mod config path
-	config_path = os.path.join(mod_sys_dir, args.mod + '.ini')
-	
-	if not os.path.isfile(config_path):
-		print "error: could not resove mod config file"
-		sys.exit(1)
+    #mod config path
+    config_path = os.path.join(mod_sys_dir, args.mod + '.ini')
 
-	#parse config
-	config = ConfigParser.RawConfigParser(dict_type=MultiOrderedDict)
-	config.read(config_path)
+    if not os.path.isfile(config_path):
+        print "error: could not resove mod config file"
+        sys.exit(1)
 
-	packages = config.get('Editor.EditorEngine', 'editpackages')
+    #parse config
+    config = ConfigParser.RawConfigParser(dict_type=MultiOrderedDict)
+    config.read(config_path)
 
-	packages_to_compile = []
+    packages = config.get('Editor.EditorEngine', 'editpackages')
 
-	for package in packages:
-		ro_sys_package_path = os.path.join(ro_sys_dir, package + '.u')
+    packages_to_compile = []
+    package_crcs = dict()
 
-		if os.path.isfile(ro_sys_package_path):
-			#compiled file exists in root system folder
-			continue
+    with open('.packagecrcs', 'r') as f:
+        package_crcs = json.load(f)
 
-		if args.clean:
-			packages_to_compile.append(package + '.u')
-			continue
+    for package in packages:
+        ro_sys_package_path = os.path.join(ro_sys_dir, package + '.u')
 
-		mod_sys_package_path = os.path.join(mod_sys_dir, package + '.u')
+        if os.path.isfile(ro_sys_package_path):
+            #compiled file exists in root system folder
+            continue
 
-		#get package modified time
-		package_mtime = 0.0
+        if args.clean:
+            packages_to_compile.append(package + '.u')
+            continue
 
-		if os.path.isfile(mod_sys_package_path):
-			package_mtime = os.path.getmtime(mod_sys_package_path)
+        mod_sys_package_path = os.path.join(mod_sys_dir, package + '.u')
 
-		should_compile_package = False
-		package_src_dir = os.path.join(ro_dir, package, 'Classes')
+        #get package modified time
+        package_mtime = 0.0
 
-		for root, dirs, files in os.walk(package_src_dir):
-			for file in files:
-				if not file.endswith('.uc'):
-					continue
+        if os.path.isfile(mod_sys_package_path):
+            package_mtime = os.path.getmtime(mod_sys_package_path)
 
-				filename = os.path.join(root, file)
-				file_mtime = os.path.getmtime(filename)
+        should_compile_package = False
+        package_src_dir = os.path.join(ro_dir, package, 'Classes')
+        package_crc = 0
 
-				if os.path.getmtime(filename) > package_mtime:
-					should_compile_package = True
-					packages_to_compile.append(package + '.u')
-					break
+        for root, dirs, files in os.walk(package_src_dir):
+            for file in files:
+                if not file.endswith('.uc'):
+                    continue
 
-			if should_compile_package:
-				break
+                filename = os.path.join(root, file)
 
-	if len(packages_to_compile) == 0:
-		print "no packages to compile"
-		return
+                with open(filename, 'rb') as f:
+                    package_crc = crc32(f.read(), package_crc)
 
-	# delete packages marked for compiling
-	for package in packages_to_compile:
-		package_path = os.path.join(mod_sys_dir, package)
+                if os.path.getmtime(filename) > package_mtime:
+                    should_compile_package = True
+                    break
 
-		if os.path.isfile(package_path):
-			try:
-				os.remove(package_path)
-			except:
-				print 'error: failed to delete file ' + package + ' (do you have the game or editor running?)'
-				sys.exit(1)
+        if package not in package_crcs or package_crcs[package] != package_crc:
+            should_compile_package = True
 
-	os.chdir(ro_sys_dir)
+        if should_compile_package:
+            packages_to_compile.append(package + '.u')
 
-	# run ucc make
-	proc = subprocess.Popen(['ucc', 'make', '-mod=' + args.mod])
-	proc.communicate()
+        package_crcs[package] = package_crc
 
-	# move compiled packages to mod directory
-	for root, dirs, files in os.walk(ro_sys_dir):
-		for file in files:
-			if file in packages_to_compile:
-				shutil.copy(os.path.join(root, file), mod_sys_dir)
-				os.remove(os.path.join(root, file))
+    with open('.packagecrcs', 'w') as f:
+        json.dump(package_crcs, f)
 
-	# run dumpint on compiled packages
-	if args.dumpint:
-		for package in packages_to_compile:
-			proc = subprocess.Popen(['ucc', 'dumpint', package, '-mod=' + args.mod])
-			proc.communicate()
+    if len(packages_to_compile) == 0:
+        print "no packages to compile"
+        return
 
-		# move localization files to mod directory
-		for root, dirs, files in os.walk(ro_sys_dir):
-			for file in files:
-				if file.replace('.int', '.u') in packages_to_compile:
-					shutil.copy(os.path.join(root, file), mod_sys_dir)
-					os.remove(os.path.join(root, file))
+    # delete packages marked for compiling
+    for package in packages_to_compile:
+        package_path = os.path.join(mod_sys_dir, package)
+
+        if os.path.isfile(package_path):
+            try:
+                os.remove(package_path)
+            except:
+                print 'error: failed to delete file ' + package + ' (do you have the game or editor running?)'
+                sys.exit(1)
+
+    os.chdir(ro_sys_dir)
+
+    # run ucc make
+    proc = subprocess.Popen(['ucc', 'make', '-mod=' + args.mod])
+    proc.communicate()
+
+    # move compiled packages to mod directory
+    for root, dirs, files in os.walk(ro_sys_dir):
+        for file in files:
+            if file in packages_to_compile:
+                shutil.copy(os.path.join(root, file), mod_sys_dir)
+                os.remove(os.path.join(root, file))
+
+    # run dumpint on compiled packages
+    if args.dumpint:
+        for package in packages_to_compile:
+            proc = subprocess.Popen(['ucc', 'dumpint', package, '-mod=' + args.mod])
+            proc.communicate()
+
+        # move localization files to mod directory
+        for root, dirs, files in os.walk(ro_sys_dir):
+            for file in files:
+                if file.replace('.int', '.u') in packages_to_compile:
+                    shutil.copy(os.path.join(root, file), mod_sys_dir)
+                    os.remove(os.path.join(root, file))
 
 if __name__ == "__main__":
    main()
