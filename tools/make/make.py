f1de3f9d276595c21cef5d4ec2cfc190acdd786a
Colin Basnett <cmbasnett@gmail.com>
2015-10-20 21:05:47 -0700

    make.py will now try to read from the default configuration if one has not been generated (mainly for the automated build system)

--- tools/make/make.py
+++ tools/make/make.py
@@ -18,7 +18,7 @@ class MultiOrderedDict(OrderedDict):
             super(OrderedDict, self).__setitem__(key, value)
 
 def main():
-    #red orchestra directory
+    # red orchestra directory
     ro_dir = os.environ.get('RODIR')
 
     if ro_dir == None:
@@ -29,46 +29,51 @@ def main():
         print 'error: environment variable RODIR is not a valid directory'
         sys.exit(1)
 
-    #red orchestra system directory
+    # red orchestra system directory
     ro_sys_dir = os.path.join(ro_dir, 'System')
 
     if not os.path.isdir(ro_sys_dir):
         print 'error: could not resolve red orchestra system directory'
         sys.exit(1)
 
-    #parse options
+    # parse options
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-mod', required=True)
     argparser.add_argument('-clean', required=False, action='store_true')
     argparser.add_argument('-dumpint', required=False, action='store_true')
-    args = argparser.parse_args()
+    args = argparser.parse_args(['-mod', 'DarkestHourDev'])
 
-    #mod directory
+    # mod directory
     mod_dir = os.path.join(ro_dir, args.mod)
 
     if not os.path.isdir(mod_dir):
         print 'error: could not resolve mod directory'
         sys.exit(1)
 
-    #mod system directory
+    # mod system directory
     mod_sys_dir = os.path.join(mod_dir, 'System')
 
     if not os.path.isdir(mod_sys_dir):
         print 'error could not resolve mod system directory'
         sys.exit(1)
 
-    #mod config path
-    config_path = os.path.join(mod_sys_dir, args.mod + '.ini')
-
-    if not os.path.isfile(config_path):
-        print "error: could not resove mod config file"
-        sys.exit(1)
-
-    #parse config
     config = ConfigParser.RawConfigParser(dict_type=MultiOrderedDict)
-    config.read(config_path)
 
-    packages = config.get('Editor.EditorEngine', 'editpackages')
+    # mod config path
+    config_path = os.path.join(mod_sys_dir, '.ini')
+
+    if os.path.isfile(config_path):
+        config.read(config_path)
+        packages = config.get('Editor.EditorEngine', 'editpackages')
+    else:
+        # mod config has not been generated, use default configuration
+        config_path = os.path.join(mod_sys_dir, 'Default.ini')
+        if os.path.isfile(config_path):
+            config.read(config_path)
+            packages = config.get('Editor.EditorEngine', '+editpackages')
+        else:
+            print "error: could not resove mod config file"
+            sys.exit(1)
 
     packages_to_compile = []
     package_crcs = dict()
@@ -85,7 +90,7 @@ def main():
         ro_sys_package_path = os.path.join(ro_sys_dir, package + '.u')
 
         if os.path.isfile(ro_sys_package_path):
-            #compiled file exists in root system folder
+            # compiled file exists in root system folder
             continue
 
         if args.clean:
@@ -94,7 +99,7 @@ def main():
 
         mod_sys_package_path = os.path.join(mod_sys_dir, package + '.u')
 
-        #get package modified time
+        # get package modified time
         package_mtime = 0.0
 
         if os.path.isfile(mod_sys_package_path):
