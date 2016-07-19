157cd5d0cb0a8b4a50118f2544e1d0b48eae866d
Colin Basnett <cmbasnett@gmail.com>
2016-07-18 19:40:58 -0700

    - Added DarkestHourDev.bat and DHEd.bat. Running these bat files will rebuild the binaries then open the game and editor, respectively. - Edited .gitignore to allow *.bat files

---
+++ System/DHEd.bat
@@ -0,0 +1,4 @@
+@echo OFF
+..\tools\make\make.py -mod DarkestHourDev ..
+echo Launching DHEd...
+start ROEd.exe -mod=DarkestHourDev -nogamma
---
+++ System/DarkestHourDev.bat
@@ -0,0 +1,4 @@
+@echo OFF
+..\tools\make\make.py -mod DarkestHourDev ..
+echo Launching DarkestHourDev...
+start RedOrchestraLargeAddressAware.exe -mod=DarkestHourDev
