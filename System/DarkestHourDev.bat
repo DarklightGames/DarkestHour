38f2ff8aef8961e16d1e0a97e826a1d387d42c45
Colin Basnett <cmbasnett@gmail.com>
2016-09-28 15:31:13 -0700

    The steam_appid.txt is now set to 1280 whenever the DarkestHourDev.bat launcher is used. This eliminates a confusing "gotcha" bit of the development environment installation.

--- System/DarkestHourDev.bat
+++ System/DarkestHourDev.bat
@@ -1,5 +1,5 @@
 @echo OFF
 cd "%~dp0"
+echo 1280 > ..\System\steam_appid.txt
 ..\tools\make\make.py -mod DarkestHourDev ..
-echo Launching DarkestHourDev...
 start RedOrchestraLargeAddressAware.exe -mod=DarkestHourDev
