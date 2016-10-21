2d4c8b81f491f69d4239c275352d74259bb428f6
Colin Basnett <cmbasnett@gmail.com>
2016-10-21 16:38:51 -0700

    Updated Steamworks SDK install script and made proper .gitignore entries for all of the resultant files.

--- tools/steamworks/install.py
+++ tools/steamworks/install.py
@@ -2,13 +2,17 @@ import os
 import urllib2
 from zipfile import ZipFile
 
-def download_sdk():
-	response = urllib2.urlopen('https://partner.steamgames.com/downloads/steamworks_sdk.zip')
-	bytes = response.read()
-	with open('steamworks_sdk.zip', 'wb') as f:
-		f.write(bytes)
-	zipfile = ZipFile('steamworks_sdk.zip')
-	zipfile.extractall()
-	os.remove('steamworks_sdk.zip')
 
-download_sdk()
+SDK_URL = 'https://partner.steamgames.com/downloads/steamworks_sdk_138a.zip'
+
+
+print 'Downloading Steamworks SDK...'
+response = urllib2.urlopen(SDK_URL)
+bytes = response.read()
+cwd = os.path.dirname(os.path.realpath(__file__))
+zip_path = os.path.join(cwd, 'steamworks_sdk.zip')
+with open(zip_path, 'wb') as f:
+    f.write(bytes)
+with ZipFile(zip_path) as zipfile:
+    zipfile.extractall()
+os.remove(zip_path)
