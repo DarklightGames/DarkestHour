9cef0c2e78a642c2874c91258b126e6f4e90f4c5
Colin Basnett <cmbasnett@gmail.com>
2016-10-24 23:40:58 -0700

    Deploy script should now work properly (haven't tested it out yet, but suspect it'll do)

--- tools/steamworks/deploy.py
+++ tools/steamworks/deploy.py
@@ -4,15 +4,21 @@ import fnmatch
 import re
 import shutil
 import sys
+import subprocess
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('dir', default='.', help='root directory')
 argparser.add_argument('-mod', required=True, help='mod name')
-args = argparser.parse_args(['../..', '-mod=DarkestHourDev'])
+argparser.add_argument('-username', required=True, help='steam username')
+argparser.add_argument('-password', required=True, help='steam password')
+argparser.add_argument('-steam_guard_code', required=False, help='steam guard code')
+args = argparser.parse_args()
 
 args.dir = os.path.abspath(args.dir)
 
-content_path = os.path.join(os.path.dirname(__file__), 'sdk', 'tools', 'ContentBuilder', 'content')
+contentbuilder_path = os.path.join(os.path.dirname(__file__), 'sdk', 'tools', 'ContentBuilder')
+content_path = os.path.join(contentbuilder_path, 'content')
+steamcmd_path = os.path.join(contentbuilder_path, 'builder', 'steamcmd.exe')
 
 if not os.path.exists(content_path):
     print 'unable to find content directory (%s)' % content_path
@@ -27,6 +33,7 @@ for fname in os.listdir(content_path):
     except OSError:
         os.remove(fpath)
 
+# find the .steaminclude file
 steaminclude_path = os.path.join(args.dir, args.mod, '.steaminclude')
 
 if not os.path.exists(steaminclude_path):
@@ -44,7 +51,7 @@ with open(steaminclude_path, 'rb') as f:
         else:
             include_patterns.append(line)
 
-# now walk directory and move files to the content directory
+# walk directory and move files to the content directory
 for root, dirs, filenames in os.walk(args.dir):
     for filename in filenames:
         is_included = False
@@ -60,14 +67,13 @@ for root, dirs, filenames in os.walk(args.dir):
                 is_included = False
         if not is_included:
             continue
-        # make sure subdiretories exist
+        # make sure subdirectories exist
         if not os.path.exists(os.path.join(content_path, os.path.dirname(relpath))):
             os.makedirs(os.path.join(content_path, os.path.dirname(relpath)))
-        # copy file to 
+        # copy file to content path
         shutil.copy(os.path.join(args.dir, relpath), os.path.join(content_path, relpath))
 
-# rename darkesthourdev to darkesthour in relevant places
-# rename folders and content (OfficialMod, Default.ini)
+# rename DarkestHourDev to DarkestHour in relevant places
 for root, dirs, filenames in os.walk(content_path):
     for dir in filter(lambda x: x == 'DarkestHourDev', dirs):
         os.rename(os.path.join(root, dir), os.path.join(root, 'DarkestHour'))
@@ -79,3 +85,10 @@ for root, dirs, filenames in os.walk(content_path):
             f.seek(0)
             f.truncate()
             f.write(c)
+
+# actually do the thing!
+args = ['steamcmd.exe', '+login', args.username, args.password, args.steam_guard_code, '+run_app_build', '../scripts/app_build_1280.vdf', '+run_app_build', '../scripts/app_build_1290.vdf', '+quit']
+p = subprocess.Popen(args, executable=steamcmd_path)
+p.wait()
+
+sys.exit(p.returncode)
