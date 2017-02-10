635c7f86785369e6cfb570dc90af3b05a1516d04
Colin Basnett <cmbasnett@gmail.com>
2017-02-09 17:34:48 -0800

    Moving the ball ahead on the i18n script. Almost got it test-ready.

--- tools/i18n/i18n.py
+++ tools/i18n/i18n.py
@@ -10,6 +10,10 @@ import ConfigParser
 from collections import OrderedDict
 from xlsxwriter import Workbook
 from xlsxwriter.utility import xl_rowcol_to_cell
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from collections import defaultdict
+import json
 
 def parse_array(s, i):
     if s[i] != '(':
@@ -34,12 +38,10 @@ def parse_array(s, i):
             # end of the list
             break
         i += 1
-        time.sleep(0.25)
     return items
 
 
 def parse_array_item(s, i):
-    #print s
     if s[i] == '\"':
         i += 1
         st = ''
@@ -51,13 +53,12 @@ def parse_array_item(s, i):
                 st += c
         return i + j + 1, st
     elif s[i] == '(':
-        raise RuntimeError('uh oh')
+        # TODO: parse struct
+        return len(s) - 1, s[i + 1:]
     else:
         return None
-        #print s
-        #raise RuntimeError(s)
 
-def read_localization_file(sections, language, path, default_language='int'):
+def read_localization_file(sections, language, path):
     config = ConfigParser.ConfigParser()
     config.optionxform = str
     config.read(path)
@@ -80,7 +81,8 @@ def read_localization_file(sections, language, path, default_language='int'):
                 match = re.match(r'^\((.+)\)$', value)
                 # TODO: split these
                 if match is None:
-                    print section_name, key, value
+                    #print section_name, key, value
+                    pass
                 else:
                     value = parse_array(value, 0)
             values = section[key]
@@ -96,7 +98,7 @@ def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument('dir', default='.', help='root directory')
     argparser.add_argument('-mod', required=True, help='mod name')
-    args = argparser.parse_args(['-mod', modname, 'D:\Steam\steamapps\common\Red Orchestra'])
+    args = argparser.parse_args(['-mod', modname, 'C:\Users\colin_000\Documents\GitHub\DarkestHour'])
 
     # TODO: prompt to do a clean recompile
     print os.path.join(args.dir, 'tools', 'make', 'make.py')
@@ -134,15 +136,17 @@ def main():
         print 'error could not resolve mod system directory'
         sys.exit(1)
 
+    workbook_name = '{0}.xlsx'.format(modname)
+
     # TODO: write to mod folder
-    workbook = Workbook('{0}.xlsx'.format(modname))
+    workbook = Workbook(workbook_name)
 
     # TODO: set up formats
     section_format = workbook.add_format({'bg_color': '#808080', 'font_color': '#ffffff'})
     value_format = workbook.add_format({'text_wrap': True})
     languages_format = workbook.add_format({'locked': True, 'bg_color': '#000000', 'font_color': '#ffffff', 'bold': True})
     key_format = workbook.add_format({'locked': True, 'bold': True, 'bg_color': '#c0c0c0'})
-    int_value_format = workbook.add_format({'bg_color': '#ffffe0'})
+    int_value_format = workbook.add_format({'bg_color': '#ffffe0', 'text_wrap': True})
     no_data_format = workbook.add_format({'bg_color': '#ffcccb'})
     data_format = workbook.add_format({'bg_color': '#ccffcb'})
 
@@ -213,13 +217,10 @@ def main():
                                 vals = sections[section][key][language]
                                 if i >= 0 and i < len(vals):
                                     worksheet.write(row, col, vals[i].decode('cp1252'))
-                                    # TODO: write it here man
-                                print vals
+                            cell = xl_rowcol_to_cell(row, col)
+                            worksheet.conditional_format(cell, {'type': 'blanks', 'format': no_data_format})
+                            worksheet.conditional_format(cell, {'type': 'no_blanks', 'format': data_format})
                         row += 1
-                                # TODO: has value, do index lookup
-                            #print langval
-                            # TODO: use i to query other language
-
                     # start_row = row
                     # for language in languages:
                     #     row = start_row
@@ -236,11 +237,83 @@ def main():
                     #             worksheet.write(row + array_row, col, v.decode('cp1252'), format)
                     #             array_row += 1
                     #         row += array_row
-                    #         #if language != 'int':
-                    #             #worksheet.conditional_format(cell, {'type': 'blanks', 'format': no_data_format})
-                    #             #worksheet.conditional_format(cell, {'type': 'no_blanks', 'format': data_format})
 
     workbook.close()
+
+    # TODO: now READ IT IN
+    workbook = load_workbook(workbook_name)
+
+    for sheet_name in workbook.get_sheet_names():
+        sheet = workbook.get_sheet_by_name(sheet_name)
+        # gather up languages in the file
+        col = 2
+        row = 1
+        languages = []
+        while True:
+            cell = '{0}{1}'.format(get_column_letter(col), row)
+            if sheet[cell].value is None:
+                break
+            languages.append(sheet[cell].value)
+            col += 1
+        # TODO: go down the rows, find the first section:
+        # sections are ones where the first column is filled, but there's nothing in the `int` column
+        row += 1
+        col = 1
+        sections = OrderedDict()
+        section = None
+        key = None
+        while True:
+            cell = 'A{0}'.format(row)
+            cell2 = 'B{0}'.format(row)
+            if sheet[cell].value is None and sheet[cell2].value is None:
+                # EOF, basically
+                break
+            if sheet[cell].value is not None and sheet[cell2].value is None:
+                # new section, add it!
+                section = OrderedDict()
+                sections[sheet[cell].value] = section
+            else:
+                # we got a key/value row on our hands here
+                if sheet[cell].value is not None:
+                    key = sheet[cell].value
+                col = 3
+                for language in languages[1:]:
+                    # TODO: go searching for the translations across the rows, do a look-up for the 
+                    cell = '{0}{1}'.format(get_column_letter(col), row)
+                    value = sheet[cell].value
+                    if value is None:
+                        continue
+                    if key not in section:
+                        # add key to section
+                        section[key] = dict()
+                        section[key][language] = list()
+                    section[key][language].append(value)
+                    col += 1
+            row += 1
+        # TODO: now write out to an .ini file
+        for language in languages[1:]:
+            with open(sheet_name + '.' + language, 'wb') as f:
+                # don't write anything if there's nothing to write!
+                for section_name in sections.keys():
+                    kvs = []
+                    for key in sections[section_name].keys():
+                        # TODO: make sure present language has anything for this key
+                        if language in sections[section_name][key]:
+                            values = sections[section_name][key][language]
+                            # TODO: check length of values, parse depending on size etc.
+                            if len(values) == 1:
+                                kvs.append((key, '"' + values[0].encode('cp1252') + '"'))
+                            else:
+                                print key
+                                kvs.append((key, '(' + ','.join(map(lambda x: '"' + x.encode('cp1252') + '"', values)) + ')'))
+                    if len(kvs) == 0:
+                        continue
+                    f.write('[{0}]\n'.format(section_name))
+                    for kv in kvs:
+                        f.write('{0}={1}\n'.format(kv[0], kv[1]))
+                    f.write('\n')
+        # s = json.dumps(sections)
+        # print s
 
 
 if __name__ == "__main__":
