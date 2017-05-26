DarklightGames/DarkestHour

commit 852206bcf65ab8282ec0fd72cbff0fd0b553d675

Scoreboard now reveals the server title at the top instead of "SCORES" which was redundant information.  * The server title is pruned after 64 characters so it isn't too long  * Title changed to be white (as black)  * Title font is smaller now

DH_Interface/Classes/DHScoreBoard.uc
@@ -198,9 +198,9 @@ simulated function UpdateScoreBoard(Canvas C)
     Y = CalcY(1.0, C);
     C.DrawTile(HeaderImage, C.ClipX, Y, 0.0, 0.0, 2048.0, 64.0);
 
-    C.DrawColor = HUD.default.BlackColor;
-    C.Font = HUD.static.GetLargeMenuFont(C);
-    C.DrawTextJustified(TitleText, 1, 0.0, 0.0, C.ClipX, Y);
+    C.DrawColor = HUD.default.WhiteColor;
+    C.Font = HUD.static.GetSmallMenuFont(C);
+    C.DrawTextJustified(Left(DHGRI.ServerName, 64), 1, 0.0, 0.0, C.ClipX, Y);
 
     // Now set standard font & line height
     C.Font = HUD.static.GetSmallerMenuFont(C);

commit c0e76028cc568294c3dc718b79611688618f6f34

Fix small bug introduced in commit: https://github.com/DarklightGames/DarkestHour/commit/852206bcf65ab8282ec0fd72cbff0fd0b553d675 ("Scoreboard now reveals the server title at the top instead of "SCORES"). It lost the drop shadow that should be applied to the scoreboard header text. Problem was by removing the block that drew "SCORES", it removed setting the DrawColor to black, which was being used later in the drop shadow block. Also it was setting the DrawColor to white twice, which isn't necessary. Fixed and added a couple of comments to explain the drop shadow.

DH_Interface/Classes/DHScoreBoard.uc
@@ -198,7 +198,6 @@ simulated function UpdateScoreBoard(Canvas C)
     Y = CalcY(1.0, C);
     C.DrawTile(HeaderImage, C.ClipX, Y, 0.0, 0.0, 2048.0, 64.0);
 
-    C.DrawColor = HUD.default.WhiteColor;
     C.Font = HUD.static.GetSmallMenuFont(C);
     C.DrawTextJustified(Left(DHGRI.ServerName, 64), 1, 0.0, 0.0, C.ClipX, Y);
 
@@ -250,14 +249,14 @@ simulated function UpdateScoreBoard(Canvas C)
     s $= HUD.default.SpacingText $ HUD.default.MapGameTypeText $ DHGRI.CurrentGameType;
 
     // Draw our round/server info line, with a drop shadow
-    C.DrawColor.A = 128; // DrawColor is already black for shadow
+    C.SetDrawColor(0, 0, 0, 128);
     X = BaseXPos[AXIS_TEAM_INDEX];
     C.SetPos(X + 1, Y + 1);
-    C.DrawTextClipped(s);
+    C.DrawTextClipped(s); // this is the dark 'drop shadow' text, slightly offset from the actual text
 
     C.DrawColor = HUD.default.WhiteColor;
     C.SetPos(X, Y);
-    C.DrawTextClipped(s);
+    C.DrawTextClipped(s); // this is the actual text, drawn over the drop shadow
 
     //////////////// DRAW AXIS TEAM ////////////////
 