DarklightGames/DarkestHour

commit 7b5ac5f876dfab0d812c2d445bf1af8e3a17a3cb

Changed the behavior of `DHSpawnPoint`s. The location of the spawn point actor itself will not be used as a fallback unless specified using `bUseLocationAsFallback` (default is `false``). If it is false, it will randomly pick a location hint to use instead.

DH_Engine/Classes/DHSpawnPoint.uc
@@ -30,6 +30,7 @@ var     array<DHLocationHint>   InfantryLocationHints; // saved references to li
 var     array<DHLocationHint>   VehicleLocationHints;
 var()   float           LocationHintDeferDistance;     // the spawn manager will try to avoid spawning players at location hints where there are enemies within this distance
                                                        // (it will defer evaluation of any location hints that have enemies nearby)
+var()   bool            bUseLocationAsFallback;        // If true, then use the location of this actor as a fallback location in the event that all location hints are blocked
 
 // Options to link various types of actors to this SP, so they are only active when the SP is active
 var()   name                    MineVolumeProtectionTag;
@@ -370,18 +371,20 @@ function bool GetSpawnPosition(out vector SpawnLocation, out rotator SpawnRotati
         }
     }
 
-    // Found a usable location hint
-    if (LocationHintIndex != -1)
+    if (LocationHintIndex == -1)
     {
-        SpawnLocation = LocationHints[LocationHintIndex].Location;
-        SpawnRotation = LocationHints[LocationHintIndex].Rotation;
+        if (bUseLocationAsFallback)
+        {
+            SpawnLocation = Location;
+            SpawnRotation = Rotation;
+            return true;
+        }
+
+        LocationHintIndex = Rand(LocationHints.Length);
     }
-    // Otherwise use spawn point itself
-    else
-    {
-        SpawnLocation = Location;
-        SpawnRotation = Rotation;
-    }
+
+    SpawnLocation = LocationHints[LocationHintIndex].Location;
+    SpawnRotation = LocationHints[LocationHintIndex].Rotation;
 
     return true;
 }

commit b875a19d6cb7d1d95a1ff2d6efa74a99d8fa7373

Fixed a bug introduced in commit 7b5ac5f8 where players would be spawned at world origin if there were no location hints.

DH_Engine/Classes/DHSpawnPoint.uc
@@ -373,7 +373,7 @@ function bool GetSpawnPosition(out vector SpawnLocation, out rotator SpawnRotati
 
     if (LocationHintIndex == -1)
     {
-        if (bUseLocationAsFallback)
+        if (LocationHints.Length == 0 || bUseLocationAsFallback)
         {
             SpawnLocation = Location;
             SpawnRotation = Rotation;