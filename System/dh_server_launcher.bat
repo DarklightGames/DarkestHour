0d3c078777acd0973948692e9ab693f756bbde04
Colin Basnett <cmbasnett@gmail.com>
2017-09-08 17:35:46 -0700

    Added the admin menu mutator to the start-line of the dh_server_launcher.bat

--- System/dh_server_launcher.bat
+++ System/dh_server_launcher.bat
@@ -1,1 +1,1 @@
-ucc server DH-Brecourt.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64? -log=DHServerLog_%RANDOM%.log -mod=DarkestHour -ini=DarkestHour.ini
+ucc server DH-Brecourt.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHour -ini=DarkestHour.ini
