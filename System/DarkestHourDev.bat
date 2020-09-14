0c79146856827a2a6fc42649a69d8d11db8771af
shiftycats <mooncats@tutanota.com>
2020-09-14 16:54:34 +0300

    Removed extraneous launch commands

--- System/DarkestHourDev.bat
+++ System/DarkestHourDev.bat
@@ -2,6 +2,4 @@
 cd "%~dp0"
 echo 1280 > ..\System\steam_appid.txt
 python ..\tools\make\make.py -mod DarkestHourDev ..
-start RedOrchestraLargeAddressAware.exe -mod=DarkestHourDev -log=DHLog_%RANDOM%.log 
-start RedOrchestraLargeAddressAware1.exe -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
-start RedOrchestraLargeAddressAware2.exe -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
+start RedOrchestraLargeAddressAware.exe -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
