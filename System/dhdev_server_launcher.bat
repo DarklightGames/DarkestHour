741f5106db4e970e17a48334ab4bce65442ac5b9
mimiski <mimiski@protonmail.com>
2021-05-30 13:28:01 +0200

    #1205 Compile files before running server and clients

--- System/dhdev_server_launcher.bat
+++ System/dhdev_server_launcher.bat
@@ -1,3 +1,13 @@
+@echo OFF
+cd "%~dp0"
+echo 1280 > ..\System\steam_appid.txt
+python ..\tools\make\make.py -mod DarkestHourDev ..
+
+if %ERRORLEVEL% GEQ 1 (
+    pause
+    exit /b %ERRORLEVEL%
+)
+
 start ucc server DH-Target_Range.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
 start RedOrchestraLargeAddressAware.exe 172.21.96.1:7757 -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
 start RedOrchestraLargeAddressAware1.exe 172.21.96.1:7757 -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
