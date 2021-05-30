82869e428bd49fe35164e8c2c2e6ef6453da1bcc
mimiski <mimiski@protonmail.com>
2021-05-31 00:35:06 +0200

    #1205 Refactoring the server launcher: - extracting changes made to dhdev_server_launcher.bat to a separate file - resetting contents of dhdev_server_launcher.bat to the previous version

---
+++ System/dhdev_server_with_clients.bat
@@ -0,0 +1,13 @@
+@echo OFF
+cd "%~dp0"
+echo 1280 > ..\System\steam_appid.txt
+python ..\tools\make\make.py -mod DarkestHourDev ..
+
+if %ERRORLEVEL% GEQ 1 (
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+start ucc server DH-WIP_Makhnovo_Advance.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
+start RedOrchestraLargeAddressAware.exe 0.0.0.0:7757 -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
+start RedOrchestraLargeAddressAware1.exe 172.21.96.1:7757 -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
