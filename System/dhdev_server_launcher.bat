b4d662a0eac919e308dd85cc058b3089bba6deca
mimiski <mimiski@protonmail.com>
2021-05-30 12:56:38 +0200

    #1205 Script for starting a DH server and automatically connecting 2 clients

--- System/dhdev_server_launcher.bat
+++ System/dhdev_server_launcher.bat
@@ -1,1 +1,3 @@
-ucc server DH-Target_Range.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
+start ucc server DH-Target_Range.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
+start RedOrchestraLargeAddressAware.exe 172.21.96.1:7757 -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
+start RedOrchestraLargeAddressAware1.exe 172.21.96.1:7757 -mod=DarkestHourDev -log=DHLog_%RANDOM%.log
