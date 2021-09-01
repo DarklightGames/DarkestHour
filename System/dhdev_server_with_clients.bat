bf50093507a242794ab60bb877efa3320b21d7fd
Colin Basnett <cmbasnett@gmail.com>
2021-09-01 01:37:42 -0700

    Added a `test_multiplayer.py` script which spins up a server and a variable number of clients (script prompts the user for the # of clients they want to spawn). Use `dhdev_server_with_clients.bat` to see this in action.
    # Conflicts:
    #	System/dhdev_server_with_clients.bat

---
+++ System/dhdev_server_with_clients.bat
@@ -0,0 +1,11 @@
+@echo OFF
+cd "%~dp0"
+echo 1280 > ..\System\steam_appid.txt
+python ..\tools\make\make.py -mod DarkestHourDev ..
+
+if %ERRORLEVEL% GEQ 1 (
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+python ..\tools\test_multiplayer.py DarkestHourDev DH-Brecourt_Push.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator
