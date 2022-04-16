f9a05b1fb503b719d18a80d479c6e436262da146
WOLFkraut <wolfkraut@gmail.com>
2022-04-16 06:33:18 -0400

    Revert "Swapped time for tickets on Ooster and fixed grenade on St Marie."
    This reverts commit 219d308589c29032b6db65e5d2f1350b923c9f1c.

--- DarkestHourDev/System/DarkestHourDev Server.bat
+++ DarkestHourDev/System/DarkestHourDev Server.bat
@@ -1,4 +1,4 @@
 @echo OFF
 cd "%~dp0"
 python ..\..\tools\make\make.py -mod DarkestHourDev ..\..
-ucc server DH-Armored_Balaton_Clash.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
+ucc server DH-Target_Range.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
--- System/dhdev_server_launcher.bat
+++ System/dhdev_server_launcher.bat
@@ -1,1 +1,1 @@
-ucc server DH-Armored_Balaton_Clash.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
+ucc server DH-Target_Range.rom?game=DH_Engine.DarkestHourGame?MaxPlayers=64?Mutator=DH_AdminMenuMutator.DHAdminMenuMutator -log=DHServerLog_%RANDOM%.log -mod=DarkestHourDev -ini=DarkestHourDev.ini
