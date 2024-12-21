27a0b886af717610f6e8000a6521e6bb17dc8c66
Yurii Ti <mooncats@tutanota.com>
2024-12-21 06:13:38 +0200

    Replaced `no-confirm` and `no-input` options with `yes` and `no-pause`
    The new naming is less ambiguous and more commonly used in other tools.

--- System/dhdev_server_with_clients.bat
+++ System/dhdev_server_with_clients.bat
@@ -11,11 +11,11 @@ echo   --map MAP          - a MAP to launch the server on (defalt: DH-Target_Ran
 echo   --clean            - compile all packages
 echo   --dumpint          - dump localization files
 echo   --safe             - don't kill UCC process when it's compiling
-echo   --no-confirm       - kill UCC process without confirmation
+echo   --yes              - answer 'yes' to all prompts
 echo   --no-kill          - don't kill UCC process
 echo   --no-build         - skip the build step
 echo   --no-run           - don't launch server and clients
-echo   --no-input         - don't wait for input on errors
+echo   --no-pause         - exit immediately without waiting for input
 echo.
 echo Other arguments are passed to 'test_multiplayer.py', including:
 echo   --clients NUM      - launch NUM clients skipping the prompt
@@ -27,11 +27,11 @@ goto :eof
 :begin
 set ucc_task=UCC.exe
 set help=0
-set no_confirm=0
+set always_yes=0
 set no_kill=0
 set no_build=0
 set no_run=0
-set no_input=0
+set no_pause=0
 set safe=0
 set lock_file=StdOut.log
 set map=DH-Target_Range
@@ -44,11 +44,11 @@ rem Flag arguments
 set arg_size=1
 
 if "%1"=="--help" set "help=1" & goto :next_arg
-if "%1"=="--no-confirm" set "no_confirm=1" & goto :next_arg
+if "%1"=="--yes" set "always_yes=1" & goto :next_arg
 if "%1"=="--no-kill" set "no_kill=1" & goto :next_arg
 if "%1"=="--no-build" set "no_build=1" & goto :next_arg
 if "%1"=="--no-run" set "no_run=1" & goto :next_arg
-if "%1"=="--no-input" set "no_input=1" & goto :next_arg
+if "%1"=="--no-pause" set "no_pause=1" & goto :next_arg
 if "%1"=="--safe" set "safe=1" & goto :next_arg
 if "%1"=="--clean" set "make_args=%make_args% -clean" & goto :next_arg
 if "%1"=="--dumpint" set "make_args=%make_args% -dumpint" & goto :next_arg
@@ -101,7 +101,7 @@ echo Can't terminate UCC while it's compiling. Remove '--safe' flag if you want 
 goto :error_exit
 
 :prompt_kill_ucc
-if %no_confirm% neq 0 goto :kill_ucc
+if %always_yes% neq 0 goto :kill_ucc
 choice /c yn /m "UCC process will be terminated. Do you wish to continue"
 if %ERRORLEVEL% neq 1 exit /b 1
 
@@ -125,5 +125,5 @@ if %ERRORLEVEL% neq 0 goto:error_exit
 goto :eof
 
 :error_exit
-if %no_input% equ 0 pause
+if %no_pause% equ 0 pause
 if %ERRORLEVEL% neq 0 (exit /b %ERRORLEVEL%) else (exit /b 1)
