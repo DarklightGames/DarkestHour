5717ca949655645eb659792505661206963ecae7
Colin Basnett <cmbasnett@gmail.com>
2025-01-22 13:31:21 -0800

    Split the font generation and localization code into different scripts

--- tools/localization/generate_fonts.bat
+++ tools/localization/generate_fonts.bat
@@ -1,6 +1,6 @@
 @echo off
 pushd %~dp0
 virtualenv venv
-.\venv\Scripts\activate.bat && pip3 install -r requirements.txt && python .\localization.py generate_font_scripts ..\..\. --mod DarkestHourDev
+.\venv\Scripts\activate.bat && pip3 install -r requirements.txt && python .\font.py generate ..\..\. --mod DarkestHourDev
 popd
 @echo on
