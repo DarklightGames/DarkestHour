fd0dace199387888aaefe198d2103e87261f7a64
Colin Basnett <5035660+cmbasnett@users.noreply.github.com>
2026-06-10 18:39:38 -0700

    Added a bit more logging for deploy script

--- tools/deploy/deploy_internal.sh
+++ tools/deploy/deploy_internal.sh
@@ -8,12 +8,15 @@ pushd "$(dirname ${BASH_SOURCE:0})" > /dev/null
 git config --global --add safe.directory /RedOrchestra
 
 # Clean localization
+echo "Cleaning localization..."
 ../localization/localization.sh clean -y
 
 # Build the game (requires wine, UCC etc.)
+echo "Building the game..."
 ../make/clean.sh
 
 # Sync localizations.
+echo "Syncing localizations"
 ../localization/localization.sh sync
 
 virtualenv venv --clear && \
